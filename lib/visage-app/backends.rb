1a1d99e30898779a5ad94a2f0ea677e44b37bea9
Dan Wanek <dan.wanek@gmail.com>
2011-03-24 21:49:12 -0500

    add zenoss backend. supporting documentation.

--- lib/visage-app/backends.rb
+++ lib/visage-app/backends.rb
@@ -1,3 +1,7 @@
+# This is a module that is the foundation of support for multiple types of RRD Backends.
+# To add a backend create a self-configuring proc to a <backendname>.rb file. See the
+# collectd or zenoss backends for examples. The actual configuration parameters should
+# go in 'lib/visage-app/config/backends.yaml'
 module Visage
   module Backends
     BACKENDS = []
@@ -5,3 +9,4 @@ module Visage
 end # Visage
 
 require 'visage-app/backends/collectd'
+require 'visage-app/backends/zenoss'
