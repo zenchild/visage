022ecba48931fd79779361f7d747b596345c1c19
Dan Wanek <dan.wanek@gmail.com>
2011-03-24 22:03:14 -0500

    add zenoss backend

---
+++ lib/visage-app/backends/zenoss/json.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby
+
+@root = Pathname.new(File.dirname(__FILE__)).parent.parent.parent.expand_path
+$: << @root.to_s
+require 'lib/visage-app/patches'
+require 'errand'
+require 'yajl'
+require 'zenoss'
+
+
+# Exposes RRDs as JSON.
+#
+# A loose shim onto RRDtool, with some extra logic to normalise the data.
+#
+class ZenossJSON
+
+  def initialize(opts={})
+    server = Visage::Config.zenoss_server
+    user   = Visage::Config.zenoss_user
+    pass   = Visage::Config.zenoss_pass
+    @zenoss = Zenoss.connect server, user, pass
+  end
+
+  # Entry point.
+  def json(opts={})
+    host             = opts[:host]
+    plugin           = opts[:plugin]
+
+    zhost = @zenoss.find_devices_by_name(host).first
+    rrd = zhost.fetch_rrd_value(plugin, (Time.now - 3600).to_datetime)
+
+    data = []
+
+    data << {
+      :host   => host,
+      :plugin => plugin,
+      :start  => rrd[:start],
+      :finish => rrd[:end],
+      :data   => rrd[:rrdvalues],
+    }
+
+    encode(data)
+  end
+
+  private
+  # Attempt to structure the JSON reasonably sanely, so the consumer (i.e. a
+  # browser) doesn't have to do a lot of computationally expensive work.
+  def encode(datas)
+
+    structure = {}
+    datas.each do |data|
+      rrd_data = data[:data]
+
+      host     = data[:host]
+      plugin   = data[:plugin]
+      instance = data[:plugin]
+      start    = data[:start].to_i
+      finish   = data[:finish].to_i
+      source   = 'ds0'
+
+      structure[host] ||= {}
+      structure[host][plugin] ||= {}
+      structure[host][plugin][instance] ||= {}
+      structure[host][plugin][instance][source] ||= {}
+      structure[host][plugin][instance][source][:start]  ||= start
+      structure[host][plugin][instance][source][:finish] ||= finish
+      structure[host][plugin][instance][source][:data]   ||= rrd_data
+
+    end
+
+    encoder = Yajl::Encoder.new
+    encoder.encode(structure)
+  end
+
+end
---
+++ lib/visage-app/backends/zenoss/rrds.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+$: << File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..'))
+
+require 'lib/visage-app/patches'
+require 'zenoss'
+
+module Visage
+  module Zenoss
+    class RRDs
+
+      class << self
+
+        def hosts(opts={})
+          server = Visage::Config.zenoss_server
+          user   = Visage::Config.zenoss_user
+          pass   = Visage::Config.zenoss_pass
+          zenoss = Zenoss.connect server, user, pass
+
+          if opts[:hosts].blank?
+            hosts = zenoss.get_devices.map {|dev| dev.name}
+          else
+            hosts = []
+            opts[:hosts].strip.split(/\s*,\s*/).each do |host|
+              zenoss.find_devices_by_name(host).each do |dev|
+                hosts << dev.name
+              end
+            end
+          end
+
+          hosts
+        end
+
+        def metrics(opts={})
+          server = Visage::Config.zenoss_server
+          user   = Visage::Config.zenoss_user
+          pass   = Visage::Config.zenoss_pass
+          zenoss = Zenoss.connect server, user, pass
+
+          if opts[:host].blank?
+            []
+          else
+            host = zenoss.find_devices_by_name(opts[:host]).first
+            datapoints  = host.get_rrd_data_points.map {|dp| dp.name}
+            case
+            when opts[:metrics].blank?
+              datapoints
+            when opts[:metrics] =~ /,/
+              selection = opts[:metrics].split(/\s*,\s*/)
+              selection & datapoints
+            else
+              [opts[:metrics]] & datapoints
+            end
+          end
+        end
+
+      end
+
+    end
+  end
+end
